2a86c2088c656a74124d57b080b42e00d10730a2
dharvi712 <itsdharvi.16@gmail.com>
2026-09-15 20:57:44 +0530

    Add searchMatrix function for 2D matrix search
    Implement search in a 2D matrix algorithm.

---
+++ leetcode-daily-challenge/15.09.26.py
@@ -0,0 +1,20 @@
+'''240 search in 2D matrix'''
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+        
+    
+        row,col=0,len(matrix[0])-1
+        while row<len(matrix) and col>=0:
+            val=matrix[row][col]
+            if val==target:
+                return True
+            elif val>target:
+                col -= 1
+            else:
+                row +=1
+        return False            
+                    
+
+        
